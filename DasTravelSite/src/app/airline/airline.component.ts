ce46b7814314edfd3856384bdfaebaa8747e1541
LordElemelon <hawke997@gmail.com>
2019-04-18 14:03:04 +0200

    Completed fully functional front end for altering airline information

--- DasTravelSite/src/app/airline/airline.component.ts
+++ DasTravelSite/src/app/airline/airline.component.ts
@@ -14,21 +14,19 @@ export class AirlineComponent implements OnInit {
 
   modifyActive = true;
 
-  addForm: FormGroup;
+  selectedAirline: Airline;
+
+  modifyAirlineForm: FormGroup;
   toAddAirline: Airline;
-  @ViewChild('addform') addFormDirective;
+  @ViewChild('modifyform') modifyAirlineFormDirective;
 
   modifyAirlineFormErrors = {
-    'name': '',
-    'address': ''
+    'name': ''
   };
 
   modifyAirlineFormValidationMessages = {
     'name': {
       'required' : 'Airline name is required'
-    },
-    'address': {
-      'required' : 'Airline address is required'
     }
   };
 
@@ -38,10 +36,64 @@ export class AirlineComponent implements OnInit {
   ) {
     LoopBackConfig.setBaseURL(baseURL);
     LoopBackConfig.setApiVersion(API_VERSION);
-    
+    this.createModifyAirlineForm();
   }
 
   ngOnInit() {
+    this.airlineservice.findOne({'where': {'name' : 'airline1'}})
+    .subscribe((airline: Airline) => {
+      this.selectedAirline = airline;
+      this.setValueModifyAirlineForm();
+    });
+  }
+
+  onValueChangedModifyAirline(data?: any) {
+    if (!this.modifyAirlineForm) {return; }
+    const form = this.modifyAirlineForm;
+    for (const field in this.modifyAirlineFormErrors){
+      if (this.modifyAirlineFormErrors.hasOwnProperty(field)){
+        this.modifyAirlineFormErrors[field] = '';
+        const control = form.get(field);
+        if (control && !control.valid) {
+          const messages = this.modifyAirlineFormValidationMessages[field];
+          for (const key in control.errors){
+            if (control.errors.hasOwnProperty(key)){
+              this.modifyAirlineFormErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  createModifyAirlineForm() {
+    this.modifyAirlineForm = this.fb.group({
+      'name': ['', Validators.required],
+      'address': '',
+      'description': '',
+      'destinations' : '',
+      'id' : ''
+    });
+
+    this.modifyAirlineForm.valueChanges
+      .subscribe(data => this.onValueChangedModifyAirline(data));
+    this.onValueChangedModifyAirline();
+  }
+
+  setValueModifyAirlineForm() {
+    this.modifyAirlineForm.setValue({
+      'name': this.selectedAirline.name,
+      'address' : this.selectedAirline.address,
+      'description' : this.selectedAirline.description,
+      'destinations' : this.selectedAirline.destinations,
+      'id' : this.selectedAirline.id
+    });
+  }
+
+  onModifyAirlineSubmit() {
+    this.selectedAirline = this.modifyAirlineForm.value;
+    this.airlineservice.updateAttributes(this.selectedAirline.id, this.selectedAirline)
+    .subscribe(result => {});
   }
 
 }
