b1ad4cf3fe96eb9f167a84144b92bfc0d592936b
LordElemelon <hawke997@gmail.com>
2019-05-03 23:50:34 +0200

    Some progress on oneway search

--- DasTravelSite/src/app/flights/flights.component.ts
+++ DasTravelSite/src/app/flights/flights.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import {LoopBackConfig, Flight, FlightApi} from '../shared/sdk';
+import {API_VERSION} from '../shared/baseUrl';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {MatSnackBar, MatTable} from '@angular/material';
+import {LoginServiceService} from '../login-service.service';
 
 @Component({
   selector: 'app-flights',
@@ -7,9 +12,152 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightsComponent implements OnInit {
 
-  constructor() { }
+  onewayFlightList: Flight[];
+  userType: string;
+
+  onewaySearchForm: FormGroup;
+  @ViewChild('onewaySearchForm') onewaySearchFormDirective;
+
+  @ViewChild('onewayFlightTable') onewayFlightTable: MatTable<any>;
+
+  isSearch: boolean;
+  
+  setClickedRow : Function;
+  selectedFlight: Flight = null;
+
+  onewaySearchFormErrors = {
+    'origin': '',
+    'destination': '',
+    'takeoffDate': ''
+  };
+
+  onewaySearchFormValidationMessages = {
+    'origin': {
+      'required': 'Origin of flight is required'
+    },
+    'destination': {
+      'required': 'Destination of flight is required'
+    },
+    'takeoffDate': {
+      'required': 'Takeoff time of flight is required'
+    }
+  };
+
+
+
+  constructor(@Inject('baseURL') private baseURL,
+              private flightservice: FlightApi,
+              private fb: FormBuilder,
+              private loginService: LoginServiceService,
+              public snackBar: MatSnackBar) {
+    LoopBackConfig.setBaseURL(baseURL);
+    LoopBackConfig.setApiVersion(API_VERSION);
+    this.createOnewaySearchForm();
+    this.loginService.user.subscribe(data => {
+      if (data) {
+        this.userType = data.user.type;
+      }
+    });
+    this.setClickedRow = function(index){
+      this.selectedFlight = index;
+    }
+  }
 
   ngOnInit() {
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+  }
+
+  onValueChangedOnewaySearch(data?: any) {
+    if (!this.onewaySearchForm) {return; }
+    const form = this.onewaySearchForm;
+    for (const field in this.onewaySearchFormErrors){
+      if (this.onewaySearchFormErrors.hasOwnProperty(field)){
+        this.onewaySearchFormErrors[field] = '';
+        const control = form.get(field);
+        if (control && !control.valid) {
+          const messages = this.onewaySearchFormValidationMessages[field];
+          for (const key in control.errors){
+            if (control.errors.hasOwnProperty(key)){
+              this.onewaySearchFormErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
+
+  createOnewaySearchForm() {
+    this.onewaySearchForm = this.fb.group({
+      'origin': ['', Validators.required],
+      'destination': ['', Validators.required],
+      'takeoffDate': ['', Validators.required]
+    });
+
+    this.onewaySearchForm.valueChanges
+      .subscribe(data => this.onValueChangedOnewaySearch(data));
+    this.onValueChangedOnewaySearch();
+  }
+
+  setValueOnewaySearchForm() {
+    this.onewaySearchForm.setValue({
+      'origin': this.selectedFlight.origin,
+      'destination': this.selectedFlight.destination,
+      'takeoffDate': this.selectedFlight.takeoffDate
+    });
+  }
+
+  getFlights(searchObject: any) {
+    this.flightservice.find({
+      'where': {
+        'origin': searchObject.origin,
+        'destination': searchObject.destination
+      }
+    })
+      .subscribe(
+        (result) => {
+          let fullFlightList: Flight[] = result as Flight[];
+          if (fullFlightList.length == 0) {
+            this.openSnackBar('No flights match search parameters', 'Dismiss');
+          }
+          else {
+            let compareDateStart: Date = searchObject.takeoffDate as Date;
+            /*if (compareDateStart.getTimezoneOffset() < 0) {
+              compareDateStart.setUTCDate(compareDateStart.getUTCDate() + 1);
+            }
+            compareDateStart.setUTCHours(0, 0, 0, 0);*/
+
+            console.log("Date to check for > " + compareDateStart.toLocaleDateString());
+
+            fullFlightList.forEach((flight) => {
+              console.log("Flight date > " + flight.takeoffDate.toDateString());
+              if (compareDateStart.toLocaleDateString() == flight.takeoffDate.toDateString()) {
+                this.onewayFlightList.push(flight);
+              }
+              this.onewayFlightTable.renderRows();
+            });
+          }
+        },
+        (err) => {
+          this.openSnackBar('Error during flights search', 'Dismiss');
+        }
+      );
+  }
+
+  onOnewaySearchSubmit() {
+    this.getFlights(this.onewaySearchForm.value);
+  }
+
+
+
+  
+
+  setToSearch() {
+    this.isSearch = true;
   }
 
 }
